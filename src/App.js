df9b8b7741e0a78b12a9e9491421fc8d8937ac71
titanprv <vaskarprasad123@gmail.com>
2020-05-18 09:45:07 +0530

    changes made to menu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menucomponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <Navbar dark color="Primary">
+    <div>
+     <Navbar color="Primary">
        <div className="container">
          <NavbarBrand href ="/">Prasad Vaskar</NavbarBrand>
        </div>
      </Navbar>
+     <Menu/>
     </div>
   );
 }
